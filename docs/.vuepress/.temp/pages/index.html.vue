bb4c18ce9db960bb00293113a9c58424e5230551
Vinzerr <vincentbuencuchillo@gmail.com>
2023-10-23 15:45:47 +0000

    Cleanup

---
+++ docs/.vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,5 @@
+<template><div><h1 id="teleflax" tabindex="-1"><a class="header-anchor" href="#teleflax" aria-hidden="true">#</a> Teleflax</h1>
+<p>A powerful, flexible and interactive framework for your Telegram BOT API.</p>
+</div></template>
+
+
---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+lang: en-US
+title: Teleflax
+description: A powerful, flexible and interactive framework for your Telegram BOT API.
+---
+
+# Teleflax
+A powerful, flexible and interactive framework for your Telegram BOT API.
